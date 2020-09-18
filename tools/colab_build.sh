9bdc193e0b9228460c58227fc9edd7eaf6c19cf0
Lanking <qingla@amazon.com>
2020-09-18 15:27:15 -0700

    fix script to allow utils loading from notebook (#44)
    * fix script to allow utils loading from notebook
    
    * fix

--- tools/colab_build.sh
+++ tools/colab_build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo "Update environment..."
-apt update -q  > /dev/null
+apt update -q  &> /dev/null
 echo "Install Java..."
-apt-get install -q openjdk-11-jdk-headless > /dev/null
+apt-get install -q openjdk-11-jdk-headless &> /dev/null
 echo "Install Jupyter java kernel..."
-curl -L https://github.com/SpencerPark/IJava/releases/download/v1.3.0/ijava-1.3.0.zip -o ijava-kernel.zip > /dev/null
-unzip -q ijava-kernel.zip -d ijava-kernel && cd ijava-kernel && python3 install.py --sys-prefix > /dev/null
+curl -L https://github.com/SpencerPark/IJava/releases/download/v1.3.0/ijava-1.3.0.zip -o ijava-kernel.zip &> /dev/null
+unzip -q ijava-kernel.zip -d ijava-kernel && cd ijava-kernel && python3 install.py --sys-prefix &> /dev/null
+git clone https://github.com/aws-samples/d2l-java &> /dev/null
+cp -r d2l-java/utils ../
