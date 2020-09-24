f45fecd9fe9b05cc063b0f8ff16a0b734e5b12dc
Qing Lan <qingla@amazon.com>
2020-09-24 11:10:06 -0700

    remove header for index.ipynb

--- tools/add_online_runner.py
+++ tools/add_online_runner.py
@@ -6,10 +6,11 @@ binder_prefix = "Run this notebook online:[![Binder](https://mybinder.org/badge_
 colab_prefix = "Colab: [![Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/aws-samples/d2l-java/blob/colab/"
 
 for file in Path('.').glob('**/*.ipynb'):
-    with open(file, mode= "r", encoding= "utf-8") as f:
-        data = json.loads(f.read())
-    with open(file, 'w') as writer:
-        constructor["source"] = [binder_prefix + str(file) + ") or " + colab_prefix + str(file) + ")"]
-        if data["cells"][0] != constructor:
-            data["cells"].insert(0, constructor)
-        writer.write(json.dumps(data))
+    if not str(file).endswith("index.ipynb"):
+        with open(file, mode= "r", encoding= "utf-8") as f:
+            data = json.loads(f.read())
+        with open(file, 'w') as writer:
+            constructor["source"] = [binder_prefix + str(file) + ") or " + colab_prefix + str(file) + ")"]
+            if data["cells"][0] != constructor:
+                data["cells"].insert(0, constructor)
+            writer.write(json.dumps(data))
