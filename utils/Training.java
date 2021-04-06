ea02ea8e5da5f2dbbde0d05282ba52ca2487d2d4
markbookk <marcos.nieves1@upr.edu>
2021-04-06 10:20:55 -0400

    Adding section 9.7

--- utils/Training.java
+++ utils/Training.java
@@ -52,6 +52,28 @@ class Training {
         }
     }
 
+    /** Clip the gradient. */
+    public static void gradClipping(Object net, int theta, NDManager manager) {
+        double result = 0;
+        NDList params;
+        params = new NDList();
+        for (Pair<String, Parameter> pair : ((AbstractBlock) net).getParameters()) {
+            params.add(pair.getValue().getArray());
+        }
+        for (NDArray p : params) {
+            NDArray gradient = p.getGradient().stopGradient();
+            gradient.attach(manager);
+            result += gradient.pow(2).sum().getFloat();
+        }
+        double norm = Math.sqrt(result);
+        if (norm > theta) {
+            for (NDArray param : params) {
+                NDArray gradient = param.getGradient();
+                gradient.muli(theta / norm);
+            }
+        }
+    }
+
     public static float accuracy(NDArray yHat, NDArray y) {
         // Check size of 1st dimension greater than 1
         // to see if we have multiple samples
