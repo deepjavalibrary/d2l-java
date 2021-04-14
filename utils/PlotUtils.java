43779d3d9ec7a4d5348b1cdd64df5f434325843b
markbookk <marcos.nieves1@upr.edu>
2021-04-14 12:36:23 -0400

    Adding another plot util function

--- utils/PlotUtils.java
+++ utils/PlotUtils.java
@@ -36,4 +36,29 @@ public class PlotUtils {
 
         return new Figure(layout, traces);
     }
+
+    public static Figure plotLineAndSegment(double[] x, double[] y, double[] segment,
+                                 String trace1Name, String trace2Name,
+                                 String xLabel, String yLabel,
+                                 int width, int height) {
+    ScatterTrace trace = ScatterTrace.builder(x, y)
+        .mode(ScatterTrace.Mode.LINE)
+        .name(trace1Name)
+        .build();
+    
+    ScatterTrace trace2 = ScatterTrace.builder(x, segment)
+        .mode(ScatterTrace.Mode.LINE)
+        .name(trace2Name)
+        .build();
+
+    Layout layout = Layout.builder()
+        .height(height)
+        .width(width)
+        .showLegend(true)
+        .xAxis(Axis.builder().title(xLabel).build())
+        .yAxis(Axis.builder().title(yLabel).build())
+        .build();
+
+    return new Figure(layout, trace, trace2);
+}
 }
