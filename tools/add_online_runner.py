5af71ec689e4bd019ef2c491a6f3fb27e125d52c
Frank Liu <frankfliu2000@gmail.com>
2023-02-03 22:27:22 -0800

    Use djl-import for homework chapter

--- tools/add_online_runner.py
+++ tools/add_online_runner.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-import glob, json
+import glob, json, os
 
 constructor = {"cell_type": "markdown", "metadata": {}}
 binder_prefix = "Run this notebook online:[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/deepjavalibrary/d2l-java/master?filepath="
 colab_prefix = "Colab: [![Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/deepjavalibrary/d2l-java/blob/colab/"
 
 for file in Path('.').glob('**/*.ipynb'):
-    if not str(file).endswith("index.ipynb"):
+    if os.path.basename(file) not in ["index.ipynb", "djl-imports.ipynb", "plot-utils.ipynb"]:
         with open(file, mode= "r", encoding= "utf-8") as f:
             data = json.loads(f.read())
         with open(file, 'w') as writer:
