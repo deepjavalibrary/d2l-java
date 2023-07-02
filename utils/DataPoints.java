f9eef460666ece766e9f3786287fc242411cb0c0
KexinFeng <fenkexin@amazon.com>
2023-07-02 11:26:52 -0700

    fix

--- utils/DataPoints.java
+++ utils/DataPoints.java
@@ -24,7 +24,7 @@ public class DataPoints {
     // Generate y = X w + b + noise
     public static DataPoints syntheticData(NDManager manager, NDArray w, float b, int numExamples) {
         NDArray X = manager.randomNormal(new Shape(numExamples, w.size()));
-        NDArray y = X.dot(w).add(b);
+        NDArray y = X.matMul(w).add(b);
         // Add noise
         y = y.add(manager.randomNormal(0, 0.01f, y.getShape(), DataType.FLOAT32));
         return new DataPoints(X, y);
